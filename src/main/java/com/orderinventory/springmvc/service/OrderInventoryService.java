1fab1665ed3e19b43398b6d98c02716e7a15ad97
sarvesh padwal <msg.pegasian@gmail.com>
2018-10-15 22:03:15 +0530

    first commit

---
+++ src/main/java/com/orderinventory/springmvc/service/OrderInventoryService.java
@@ -0,0 +1,9 @@
+package com.orderinventory.springmvc.service;
+
+import com.orderinventory.springmvc.model.Order;
+
+public interface OrderInventoryService {
+
+	public void processOrder(Order order);
+	
+}
